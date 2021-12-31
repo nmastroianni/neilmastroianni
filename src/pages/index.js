b02122f65a051933e84d52341483e292e8f0c37f
Neil Mastroianni <suancho@gmail.com>
2021-12-31 05:51:56 -0500

    adjusting mobile padding and fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import { FaArrowDown, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa"
 
 export default function Home() {
   return (
-    <div className="min-h-screen mx-auto px-16 dark:bg-slate-900 flex items-center justify-center">
+    <div className="min-h-screen px-4 md:px-8 lg:px-16 dark:bg-slate-900 flex items-center justify-center">
       <div className="w-full max-w-lg relative">
         <div className="absolute top-0 -left-4 w-72 h-72 bg-teal-300 dark:bg-stone-200 rounded-full mix-blend-multiply dark:mix-blend-overlay mix blur-xl opacity-70 dark:opacity-95 animate-blob"></div>
         <div className="absolute top-0 -right-4 w-72 h-72 bg-sky-200 dark:bg-slate-100 rounded-full mix-blend-multiply dark:mix-blend-overlay blur-xl opacity-70 dark:opacity-95 animate-blob animation-delay-2000"></div>
         <div className="absolute -bottom-8 left-20 w-72 h-72 bg-indigo-300 dark:bg-gray-300 rounded-full mix-blend-multiply dark:mix-blend-overlay blur-xl opacity-70 dark:opacity-95 animate-blob animation-delay-4000"></div>
-        <div className="m-8 relative space-y-4">
-          <div className="p-5 bg-opacity-90 bg-white text-teal-800 dark:bg-slate-800 dark:text-sky-100 rounded-lg flex items-center justify-between space-x-8">
-            <div className="flex-1 prose">
+        <div className="lg:m-8 relative space-y-4">
+          <div className="p-5 lg:p-8 bg-opacity-90 bg-white text-teal-800 dark:bg-slate-800 dark:text-sky-100 rounded-lg flex items-center justify-center">
+            <div className="flex-0 font-bold text-2xl sm:text-3xl md:text-4xl ">
               <h1 className="text-center text-indigo-900 dark:text-white">
                 Neil Mastroianni
               </h1>
