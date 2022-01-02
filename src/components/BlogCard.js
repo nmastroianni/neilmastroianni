0f5971b628261a9646d57b33931308302be35728
Neil Mastroianni <suancho@gmail.com>
2022-01-02 09:34:19 -0500

    preparing blog card

---
+++ src/components/BlogCard.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+export default function BlogCard({ data }) {
+  return (
+    <div className="">
+      <h2>Blog Card</h2>
+    </div>
+  )
+}
