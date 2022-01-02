4bf4e5961a2766f791a4c32501066855f02eff54
Neil Mastroianni <suancho@gmail.com>
2022-01-02 09:29:01 -0500

    including layout

--- src/pages/blog/{wpPost.uri}.js
+++ src/pages/blog/{wpPost.uri}.js
@@ -1,26 +1,29 @@
 import * as React from "react"
-import {graphql} from "gatsby"
+import { graphql } from "gatsby"
+import Layout from "../../components/Layout"
 
-
-export default function WpPost({data}) {
-    console.log(data);
-    return (
-        <div className="prose">
-            <h1>Post Title</h1>
-            <div dangerouslySetInnerHTML={{__html: data.wpPost.content}} className="prose prose-2xl"/>
-        </div>
-    )
+export default function WpPost({ data }) {
+  console.log(data)
+  return (
+    <Layout>
+      <div className="prose dark:prose-invert">
+        <h1>Post Title</h1>
+        <div
+          dangerouslySetInnerHTML={{ __html: data.wpPost.content }}
+          className="prose prose-2xl dark:prose-invert"
+        />
+      </div>
+    </Layout>
+  )
 }
 
 export const query = graphql`
-
-query PostQuery($id: String) {
-    wpPost(id: {eq: $id}) {
-        content
-        slug
-        title
-        uri
+  query PostQuery($id: String) {
+    wpPost(id: { eq: $id }) {
+      content
+      slug
+      title
+      uri
     }
-}
-
+  }
 `
