99866287c7e3ac0f10b229eec4b349fe52751bbf
Neil Mastroianni <suancho@gmail.com>
2022-01-03 06:09:12 -0500

    replacing p with div

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -3,20 +3,22 @@ import { Link } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 export default function BlogCard({ data }) {
-  console.log(data)
   return (
     <div className="max-w-xl mx-auto rounded border border-sky-700">
       {data.featuredImage && (
-        <GatsbyImage
-          image={getImage(
-            data.featuredImage.node.localFile.childImageSharp.gatsbyImageData
-          )}
-          className="block mx-auto aspect-w-16 aspect-h-9"
-        />
+        <div className="">
+          <GatsbyImage
+            image={getImage(
+              data.featuredImage.node.localFile.childImageSharp.gatsbyImageData
+            )}
+            className="block mx-auto"
+            alt=""
+          />
+        </div>
       )}
-      <div className="p-3 md:p-4 lg:p-6">
+      <div className="p-3 md:p-4 lg:p-6 prose prose-sky md:prose-lg">
         <h2 className="text-center text-2xl">{data.title}</h2>
-        <p dangerouslySetInnerHTML={{ __html: data.excerpt }}></p>
+        <div dangerouslySetInnerHTML={{ __html: data.excerpt }}></div>
       </div>
     </div>
   )
